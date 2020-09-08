febab072f13f18f298863ca6874886139bc4954d
jaiverma <jai2.verma@outlook.com>
2020-09-08 19:52:06 +0530

    Moving some files around

--- 3.1/3.1.3/sum/sum_test.ml
+++ 3.1/3.1.3/sum/sum_test.ml
@@ -1,6 +1,10 @@
 open OUnit2
 open Sum
 
+let make_sum_test name expected_output input =
+    name >:: (fun _ -> assert_equal expected_output (sum_fail input)
+        ~printer:string_of_int)
+
 let tests = "test suite for sum" >::: [
     "empty"  >:: (fun _ -> assert_equal 0 (sum []));
     "one"    >:: (fun _ -> assert_equal 1 (sum [1]));
@@ -8,10 +12,10 @@ let tests = "test suite for sum" >::: [
 ]
 
 let tests_fail = "test suite for sum_fail" >::: [
-    "empty2" >:: (fun _ -> assert_equal 1 (sum_fail []));
-    "empty"  >:: (fun _ -> assert_equal 0 (sum_fail []));
-    "one"    >:: (fun _ -> assert_equal 1 (sum_fail [1]));
-    "onetwo" >:: (fun _ -> assert_equal 3 (sum_fail [1;2]));
+    make_sum_test "empty2" 1 [];
+    make_sum_test "empty" 0 [];
+    make_sum_test "one" 1 [1];
+    make_sum_test "onetwo" 3 [1; 2];
 ]
 
 (* let _ = run_test_tt_main tests *)
